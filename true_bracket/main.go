6e6ba696d40e4b2bb3183826ec26008f37b543e8
reneforever <464637243@qq.com>
2021-10-13 22:25:27 +0800

    feat: one day a little

---
+++ true_bracket/main.go
@@ -0,0 +1,31 @@
+package main
+
+func isBracket(str string) bool {
+	var stack []string
+	strMap := map[string]string{
+		")": "(",
+		"]": "[",
+		"}": "{",
+	}
+	for _, v := range str {
+		s := string(v)
+		_, ok := strMap[str]
+		if ok {
+			if len(stack) == 0 {
+				return false
+			}
+			if strMap[str] != stack[len(stack)-1] {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, s)
+		}
+	}
+	if len(stack) != 0 {
+		return false
+	}
+	return true
+}
+
+func main() {}
