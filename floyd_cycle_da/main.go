e59e851685f568a22ca9363a247e850ddde40321
reneforever <464637243@qq.com>
2022-12-25 11:30:03 +0800

    add floyd-cycle

--- floyd_cycle_da/main.go
+++ floyd_cycle_da/main.go
@@ -1,3 +1,35 @@
-package floyd_cycle_da
+package main
 
-// floyd判圈算法
+import "fmt"
+
+type NodeList struct {
+	Value int
+	Next  *NodeList
+}
+
+func hasCycle(head *NodeList) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+	slow := head
+	quick := head
+	for quick != nil && quick.Next != nil {
+		slow = slow.Next
+		quick = quick.Next.Next
+
+		if quick == slow {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	node5 := NodeList{5, nil}
+	node4 := NodeList{4, &node5}
+	node3 := NodeList{3, &node4}
+	node2 := NodeList{2, &node3}
+	node1 := NodeList{1, &node2}
+	node5.Next = &node3
+	fmt.Println(hasCycle(&node1))
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module reneforevercn.c
+module reneforever_longtime_lec
 
-go 1.17
+go 1.19
